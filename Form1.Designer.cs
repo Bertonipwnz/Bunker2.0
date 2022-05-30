9560435f95e089337521cbaa90d78748af64e65a
Bertonipwnz <romashev14@mail.ru>
2022-05-30 19:25:35 +0700

    Complete refactoring of the code, putting it into different classes, creating functions and new mechanics

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿using System;
+
+namespace Бункер2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите количество игроков";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(370, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 133);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(406, 124);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Катастрофа:";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(135, 263);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(135, 38);
+            this.button12.TabIndex = 22;
+            this.button12.Text = "Сгенерировать новую катострофу:";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(661, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Бункер:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(520, 48);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(334, 243);
+            this.richTextBox2.TabIndex = 24;
+            this.richTextBox2.Text = "";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(620, 297);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(135, 38);
+            this.button13.TabIndex = 25;
+            this.button13.Text = "Сгенирировать новый бункер:";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Профессия",
+            "Пол",
+            "Возраст",
+            "Здоровье",
+            "Фобия",
+            "Хобби",
+            "Багаж",
+            "Человеческая черта",
+            "Дополнительная информация",
+            "Телосложение",
+            "Карточка действий"});
+            this.listBox1.Location = new System.Drawing.Point(33, 384);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 17);
+            this.listBox1.TabIndex = 27;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Введите номер игрока:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "Сменить характеристику";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Новая профессия",
+            "Новый пол",
+            "Новый возраст",
+            "Новое состояние здоровья",
+            "Новая фобия",
+            "Новое хобби",
+            "Новый багаж",
+            "Новая человеческая черта",
+            "Новое телосложение",
+            "Новая доп инфа",
+            "Новая карточка действий"});
+            this.listBox2.Location = new System.Drawing.Point(620, 409);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(116, 17);
+            this.listBox2.TabIndex = 37;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(620, 430);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 23);
+            this.button5.TabIndex = 38;
+            this.button5.Text = "Сменить всем";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 358);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 39;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 40;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(879, 543);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DSharpPlus;
+using Бункер2._0.Классы;
+
+namespace Бункер2._0
+{
+
+    /// <summary>
+    /// Игра написана по мотивам настольной игры "Бункер"
+    /// Характеристики генерируется рандомно и записываются в файл, с номером игрока
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        // //глобальный переменные
+        int kolvo = 0; //Инициализация переменной для количества игроков
+        string Put; //Инициализация переменной для хранения ссылки на путь к файлам игроков
+        readonly Random rnd = new Random(); //иницилизируем переменную типа рандом и выделяем для неё память в куче
+        readonly Game character = new Game(); //ссылка на объект в куче и создание экземпляра класса
+        //массив характеристик для записи
+        string[] oborudovano;
+        string[] estInBunker;
+        string[] proffesionRnd;
+        string[] healthRnd;
+        string[] phobiaRnd;
+        string[] hobbyRnd;
+        string[] HTR;
+        string[] luggageRnd;
+        string[] addInfoRnd;
+        string[] kartochka1Rnd;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog(); //вызов окна с обзором пути
+            Put = folderBrowserDialog1.SelectedPath; //считываем путь к папке
+            kolvo = Convert.ToInt32(textBox1.Text); //конвертируем поле с кол-вом игроков считывающихся с textBox1
+            string filepath; //переменная для пути к папке
+            proffesionRnd = character.ShuffleAndRandom(Game.profession.Length, Game.profession);
+            healthRnd = character.ShuffleAndRandom(Game.health.Length, Game.health);
+            phobiaRnd = character.ShuffleAndRandom(Game.phobia.Length, Game.phobia);
+            hobbyRnd = character.ShuffleAndRandom(Game.hobby.Length, Game.hobby);
+            HTR = character.ShuffleAndRandom(Game.humanTraitRand.Length, Game.humanTraitRand);
+            luggageRnd = character.ShuffleAndRandom(Game.luggage.Length, Game.luggage);
+            addInfoRnd = character.ShuffleAndRandom(Game.additionalInformation.Length, Game.additionalInformation);
+            kartochka1Rnd = character.ShuffleAndRandom(Game.kartochka1.Length, Game.kartochka1);
+            oborudovano = character.ShuffleAndRandom(Game.oborudovanieBunker.Length, Game.oborudovanieBunker);
+            estInBunker = character.ShuffleAndRandom(Game.estInBunker.Length, Game.estInBunker);
+            string[] gender = Game.gender;
+            int shkolnikID = rnd.Next(1, kolvo); //переменная для формирования школьника
+            int dedID = rnd.Next(1, kolvo); //переменная для формирования деда/бабушки
+            while (shkolnikID == dedID) //цикл на проверку совпадений номеров игрококов, если повторяется вызываем рандом ещё раз
+            {
+                shkolnikID = rnd.Next(1, kolvo);
+                dedID = rnd.Next(1, kolvo);
+            }
+            //цикл формирование документов и записи в них данных игроков
+            for (int numberplayer = 1, counter = 0, kart = kolvo; numberplayer < kolvo + 1; kart++, numberplayer++, counter++)//номер игрока, счётчик данных, чтобы не было повторений kart;
+            {
+                filepath = folderBrowserDialog1.SelectedPath + @"\" + numberplayer + ".txt"; //записываем в переменную номер игрока - номер документа
+                StreamWriter f = new StreamWriter(filepath, false); //создаём экземпляр и передаём filepath, false - создаёт новый документ если нету
+                int age = character.age(23, 50); //вызываем функцию для определения возраста и записываем в переменную age
+                if (numberplayer == shkolnikID || numberplayer == dedID) //формирования отдельных карточек среди общих для школьника/деда
+                {
+                    if (numberplayer == shkolnikID)
+                    {
+                        //школьник
+                        int shkolaAge = character.age(14, 18);
+                        f.WriteLine("Желание работать или работает(на ваш выбор): " + proffesionRnd[counter]);
+                        f.WriteLine("Пол: " + gender[rnd.Next(0, 2)]);
+                        f.WriteLine("Возраст: " + shkolaAge + " лет");
+                        f.WriteLine("Пассивка школьника(вы не можете выжить без бабушки/дедушки) под номером: " + dedID);
+                        f.WriteLine("Багаж школьника+: " + luggageRnd[kart]);
+                        f.WriteLine("Хобби: " + hobbyRnd[counter] + ". Стаж хобби: " + character.expHobby(shkolaAge) + " лет");
+                    }
+                    else
+                    {
+                        //дед/бабушка
+                        int starikAge = character.age(60, 95);
+                        f.WriteLine("Профессия: " + proffesionRnd[counter] + " Стаж работы: " + character.expDedWork(starikAge));
+                        f.WriteLine("Профессия 2: " + proffesionRnd[kart] + " Стаж работы: " + character.expDedWork(starikAge));
+                        f.WriteLine("Пол: " + gender[rnd.Next(0, 2)]);
+                        f.WriteLine("Возраст: " + starikAge + " лет");
+                        f.WriteLine("Ваш внук/внучка под номером: " + shkolnikID);
+                        f.WriteLine("Хобби: " + hobbyRnd[counter] + ". Стаж хобби: " + character.expHobby(starikAge) + " лет");
+                    }
+                }
+                else
+                {
+                    //для остальных игроков
+                    f.WriteLine("Профессия: " + proffesionRnd[counter] + " Стаж работы: " + character.expWork(age));
+                    f.WriteLine("Пол: " + gender[rnd.Next(0, 2)]);
+                    f.WriteLine("Возраст: " + age);
+                    f.WriteLine("Хобби: " + hobbyRnd[counter] + ". Стаж хобби: " + character.expHobby(age) + " лет");
+                }
+                //характеристики для всех
+                f.WriteLine(character.Health(healthRnd[counter]));
+                f.WriteLine("Фобия: " + phobiaRnd[counter]);
+                f.WriteLine("Человеческая черта: " + HTR[counter]);
+                f.WriteLine("Багаж: " + luggageRnd[counter]);
+                f.WriteLine("Телосложение: " + character.RashetIMT(healthRnd[counter]));
+                f.WriteLine("Дополнительная информация: " + addInfoRnd[counter]);
+                f.WriteLine("Карточка действий 1: " + kartochka1Rnd[counter]);
+                f.WriteLine("Карточка действий 2: " + kartochka1Rnd[kart]);
+                f.Close(); //закрываем документ
+                character.VragDrug(filepath);//вызываем метод для генерация карточек врага/друга
+            }
+            richTextBox1.Text = character.Disaster(); //записываем в richTextBox1 выход метода Disaster
+            richTextBox2.Text = character.Bunker(oborudovano, estInBunker); //записываем в richTextBox2 выход метода Bunker с входными параметрами
+        }
+
+        //метод для изменения определённой хар-ки определённого игрока при нажатии на кнопку
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string numberplayer = textBox2.Text; //считываем с textBox2 номер игрока
+            string sItem = System.Convert.ToString(listBox1.SelectedItem); //считывваем выбранное значение
+            character.OneSwapCharacter(sItem, Put, numberplayer); //передаём значение, путь к файлу, номер игрока
+        }
+
+        //перегенеровать катастрофу
+        private void button12_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = character.Disaster();//записываем в richTextBox1 выход метода Disaster
+        }
+
+        //перегенировать бункер
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //записываем в richTextBox2 выход метода Bunker с входными параметрами
+            richTextBox2.Text = character.Bunker(character.ShuffleAndRandom(oborudovano.Length, oborudovano), character.ShuffleAndRandom(estInBunker.Length, estInBunker));
+        }
+
+        //Смена всех хар-к
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string sItem = System.Convert.ToString(listBox2.SelectedItem);//считывваем выбранное значение
+            character.AllSwapCharacter(sItem, Put, kolvo); //передаём значение, путь к папке, кол-во игроков
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void  button3_Click(object sender, EventArgs e)
+        {
+           // System.Diagnostics.Process.Start(@"C:\Users\User\source\repos\Bunker3.1\bin\Release\Bunker3.1.exe");
+        }
+
+
+    }
+}
